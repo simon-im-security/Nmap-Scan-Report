b91150e5591a014571a6453bc9df70531a3d27e1
Simon .I <162538326+simon-im-security@users.noreply.github.com>
2024-04-04 09:09:48 +1100

    Update Nmap Scan Report_Mac.sh

--- Nmap Scan Report_Mac.sh
+++ Nmap Scan Report_Mac.sh
@@ -4,7 +4,7 @@
 echo "-----------------------------------------"
 echo "           Nmap Scan Report              "
 echo "-----------------------------------------"
-echo "Description: This script allows you to perform various Nmap scans, including a comprehensive port scan, a basic ping scan, and options to explore additional information on macOS ports used by Apple."
+echo "Description: This script allows you to perform various Nmap scans, including a TCP connect scan, a basic ping scan, and version detection."
 echo "Version: 2.3"
 echo "Made by Simon Im"
 echo "Date: 3rd April 2024"
@@ -26,6 +26,27 @@ if ! command -v xsltproc &> /dev/null; then
     error_exit "xsltproc is not installed. You need to install it to generate HTML reports."
 fi
 
+# Function to display scan description
+display_scan_description() {
+    case $1 in
+        1)
+            echo "1. TCP Connect Scan"
+            echo "   Description: This scan establishes a full TCP connection to the target ports."
+            ;;
+        2)
+            echo "2. Basic Ping Scan"
+            echo "   Description: This scan determines which hosts are online by sending ICMP echo requests (pings)."
+            ;;
+        3)
+            echo "3. Version detection"
+            echo "   Description: This scan detects versions of services running on open ports."
+            ;;
+        *)
+            echo "Invalid choice. Please enter a valid option."
+            ;;
+    esac
+}
+
 # Prompt user to choose between loopback or IP range
 while true; do
     echo "Choose an option:"
@@ -37,21 +58,17 @@ while true; do
     case $choice in
         1)
             target="127.0.0.1"
-            
-            # Check if the system is macOS
-            if [ "$(uname)" == "Darwin" ]; then
-                # Ask the user if they would like more information on macOS ports used by Apple
-                read -p "Before scanning, would you like more information on macOS ports used by Apple? (yes/no): " apple_choice
-                # Convert the user input to lowercase for case-insensitive comparison
-                apple_choice_lowercase=$(echo "$apple_choice" | tr '[:upper:]' '[:lower:]')
-                if [[ $apple_choice_lowercase =~ ^[y](es)?$ ]]; then
-                    open "https://support.apple.com/en-au/103229"
-                fi
-            fi
             break
             ;;
         2)
-            read -p "Enter the IP range to scan in CIDR notation (e.g., 192.168.1.0/24): " target
+            while true; do
+                read -p "Enter the IP range to scan in CIDR notation (e.g., 192.168.1.0/24): " target
+                if [ -z "$target" ]; then
+                    echo "CIDR range cannot be empty. Please provide a valid CIDR range."
+                else
+                    break
+                fi
+            done
             break
             ;;
         0)
@@ -67,62 +84,25 @@ done
 # Prompt user to choose Nmap scan type
 while true; do
     echo "Choose Nmap scan type:"
-    echo "1. TCP SYN scan (requires root)"
-    echo "2. UDP scan (requires root)"
-    echo "3. OS detection (requires root)"
-    echo "4. All (comprehensive, requires root)"
-    echo "5. Basic Ping Scan"
-    echo "6. TCP Connect scan"
-    echo "7. Version detection"
+    display_scan_description 1
+    echo ""
+    display_scan_description 2
+    echo ""
+    display_scan_description 3
+    echo ""
     echo "0. Exit"
     read -p "Enter your choice: " scan_choice
 
     case $scan_choice in
         1)
-            scan_type="-sS"
-            # Check if the user has root privileges for TCP SYN scan
-            if [ "$(id -u)" != "0" ]; then
-                echo "This scan requires root privileges."
-                continue
-            fi
-            break
-            ;;
-        2)
-            scan_type="-sU"
-            # Check if the user has root privileges for UDP scan
-            if [ "$(id -u)" != "0" ]; then
-                echo "This scan requires root privileges."
-                continue
-            fi
-            break
-            ;;
-        3)
-            scan_type="-O"
-            # Check if the user has root privileges for OS detection scan
-            if [ "$(id -u)" != "0" ]; then
-                echo "This scan requires root privileges."
-                continue
-            fi
-            break
-            ;;
-        4)
-            # Check if the user has root privileges for any of the scans in the "All" option
-            if [ "$(id -u)" != "0" ]; then
-                echo "This scan requires root privileges."
-                continue
-            fi
-            scan_type="-sS -sU -sV -O"
-            break
-            ;;
-        5)
-            scan_type="-sn"
-            break
-            ;;
-        6)
             scan_type="-sT"
             break
             ;;
-        7)
+        2)
+            scan_type="-sn"
+            break
+            ;;
+        3)
             scan_type="-sV"
             break
             ;;
@@ -131,11 +111,7 @@ while true; do
             exit 0
             ;;
         *)
-            if [[ -z $scan_choice ]]; then
-                echo "Invalid input. Please try again."
-            else
-                echo "Invalid choice. Please enter a valid option."
-            fi
+            echo "Invalid choice. Please enter a valid option."
             ;;
     esac
 done
